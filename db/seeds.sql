15824bf49707e3e88662ba1afb2a7df81c7c034e
David Thompson <drthusker@gmail.com>
2018-02-20 19:58:06 -0700

    init

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Western Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Burger');
+
+
